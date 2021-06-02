58bae388c0e69674c327cb6b1f53c8357fdc3cd3
juno <juno7803@naver.com>
2021-06-02 16:02:52 +0900

    feat: synthesis 추가 - background image 받아서 28*28 size로 수정 후 합성 - 원고지, 노트 등 다양한 배경 합성 가능

--- change.py
+++ change.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
 filename = './file/9.png'
+background_1 = './file/background_1.png'
 
-def bluring():
-    img = cv2.imread(filename)
+def bluring(source):
+    img = cv2.imread(source)
     kernel = np.ones((5, 5), np.float32)/25
     blur = cv2.filter2D(img, -1, kernel)
     cv2.imshow("original", img)
@@ -12,8 +14,8 @@ def bluring():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def coloring():
-    img = cv2.imread(filename)
+def coloring(source):
+    img = cv2.imread(source)
     yuv_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
     cv2.imshow('yuv img', yuv_img)  # color rgb -> bgr
     cv2.waitKey(0)
@@ -24,4 +26,32 @@ def coloring():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-coloring()
+def rotate(source):
+    image = cv2.imread(source)
+    # 행과 열 정보만 저장합니다.
+    height, width = image.shape[:2]
+
+    M = cv2.getRotationMatrix2D((width / 2, height / 2), 90, 0.5)
+    dst = cv2.warpAffine(image, M, (width, height))
+
+    plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+def synthesis(source, background):
+    background_image = cv2.imread(background)
+    background_resize = cv2.resize(background_image, dsize=(28, 28), interpolation=cv2.INTER_AREA)
+    # image resize 확인
+    cv2.imshow('background_resize', background_resize)
+    cv2.waitKey(0)
+
+    # image load
+    source_image = cv2.imread(source)
+
+    result = cv2.add(source_image, background_image)
+    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+# bluring(filename)
+# rotate(filename)
+# coloring(filename)
+# synthesis(filename, background_1)
